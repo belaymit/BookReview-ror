113be2b6b69debedfebfa4519e7b48ac0638a432
belaymit <2belamit@gmail.com>
2023-04-03 13:00:52 +0300

    Modified Books controlle

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,35 @@
 class BooksController < ApplicationController
   before_action :find_book, only: [:show, :edit, :update, :destroy]
   def index
-    @books = Book.all.order("created_at DESC")
+    if params[:category].blank?
+      @books = Book.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name:params[:category]).id
+      @books = Book.where(:category_id => @category_id).order("created_at DESC")
+    end
   end
   def show 
   end
 
   def new 
     @book = current_user.books.build
+    @categories = Category.all.map{ |c| [c.name, c.id]}
   end
   def create
     @book = current_user.books.build(book_params)
+    @book.category_id = params[:category_id]
     if @book.save
       redirect_to root_path
     else
       render 'new'
     end
   end
-  def edit 
+  def edit
+    @categories = Category.all.map{ |c| [c.name, c.id]}
   end
 
   def update
+    @book.category_id = params[:category_id]
     if @book.update(book_params)
       redirect_to book_path(@book)
     else
@@ -35,7 +44,7 @@ class BooksController < ApplicationController
 
   private 
   def book_params
-    params.require(:book).permit(:title, :description, :author)
+    params.require(:book).permit(:title, :description, :author, :category_id)
   end
   def find_book
     @book = Book.find(params[:id])
